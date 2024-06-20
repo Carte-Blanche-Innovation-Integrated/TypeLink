35d2a7c06bf16724ece27813a2546dbaa304fd57
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2024-06-20 10:40:10 +0500

    fixed minor issues in the app also added a gitignore

--- server/backend/urls.py
+++ server/backend/urls.py
@@ -4,7 +4,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('myapp.urls')),
+    path('api/v1/', include('warehouse.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
 ]
