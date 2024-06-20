988f080ff88721bd38d49676c4bca9be697f8613
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2024-06-20 16:52:23 +0500

    Added usage of types in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import {fetchItems} from './api'
-
+import './App.css';
+import { useEffect, useState } from 'react';
+import { fetchItems } from './api';
+import {Item} from './api-types/components/schemas.d';
 
 function App() {
-  const [count, setCount] = useState(0)
-  fetchItems()
+  const [items, setItems] = useState<Item[] | undefined> ([]);
+
+  useEffect(() => {
+    fetchItems().then((data) => {
+      setItems(data);
+    });
+  }, []);
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div id="root">
+      <h1>Warehouse Inventory</h1>
+      {items && items?.length > 0 ? (
+        <div className="card">
+          {items.map((item) => (
+            <div key={item.id} className="item">
+              <h2>{item.name}</h2>
+              <p>{item.description}</p>
+              <p>Price: ${item.price}</p>
+              <p>Stock: {item.stock}</p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p className="no-data">No data found</p>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
